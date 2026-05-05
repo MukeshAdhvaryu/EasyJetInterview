b0ddc25369515aa11796cab102896fc19ee163cd
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2026-05-05 10:26:10 +0100

    feat: Add a failing test

--- KnightTour.Tests/KnightTour.Tests.cs
+++ KnightTour.Tests/KnightTour.Tests.cs
@@ -1,4 +1,9 @@
-﻿namespace KnightTour.Tests;
+﻿using NUnit.Framework;
+using Shouldly;
+
+using KnightTour;
+
+namespace KnightTour.Tests;
 
 public class Tests
 {
@@ -8,8 +13,12 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void Finder_FindsTour()
     {
-        Assert.Pass();
+        KnightTour.IKnightTourFinder finder = new KnightTour.KnightTourFinder();
+        var result = finder.FindTour(new Square(0, 0));
+
+        result.Success.ShouldBe(true);
+        result.Path.Count.ShouldBe(64);
     }
 }
